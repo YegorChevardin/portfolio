6520fbd299312e1b068ff3fb5ee9f71ea5244302
yegorchevardin <egor03052004@gmail.com>
2022-08-28 14:22:31 +0200

    everything is finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -30,5 +21,7 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+
 
 });
